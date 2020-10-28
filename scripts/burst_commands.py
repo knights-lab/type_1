d40f4555c3d6f97ee17365d24962140ef7f6a4f5
bhillmann <hillmannben@gmail.com>
2020-10-28 08:28:08 -0500

    fixed the commit file

--- scripts/burst_commands.py
+++ scripts/burst_commands.py
@@ -5,7 +5,7 @@ import os
 # %%
 base_path = "/project/flatiron2/ben/projects/type_1/data"
 
-burst_base_directory = "/project/flatiron/data/shogun/rep201_ab/burst/refseq_s3333_d301"
+burst_base_directory = "/project/flatiron/data/shogun/rep202_ab/burst/combined_seqs"
 
 query_files = glob(f"{base_path}/mock_communities/**/filter/combined_seqs.fna", recursive=True)
 
@@ -23,22 +23,5 @@ for query_file in query_files:
   -fr \
   -i 0.98 \
   --noprogress \
-  > {base_query_path}/allpath.r202.log 2>&1
-"""
-    print(burst_command)
-for query_file in query_files:
-    base_query_path = os.path.dirname(query_file)
-
-    burst_command = f"""/usr/bin/time -v burst15 \
-  -t 72 \
-  -q {query_file} \
-  -a {burst_base_directory}.acx \
-  -r {burst_base_directory}.edx \
-  -o {base_query_path}/allpath.r201.b6 \
-  -m ALLPATHS \
-  -fr \
-  -i 0.98 \
-  --noprogress \
-  > {base_query_path}/allpath.r201.log 2>&1
-"""
+  > {base_query_path}/allpath.r202.log 2>&1"""
     print(burst_command)
