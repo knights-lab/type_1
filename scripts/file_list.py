530fa4e4caceea960f1571195bc8f108ad6c120a
bhillmann <hillmannben@gmail.com>
2020-10-29 13:16:40 -0500

    added an extra white space

---
+++ scripts/file_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from glob import glob
+import os
+import re
+
+# %%
+base_path = "/project/flatiron2/ben/projects/type_1/data"
+
+query_files = glob(f"{base_path}/mock_communities/**/filter/allpath.r202.b6", recursive=True)
+
+# %%
+for query_file in query_files:
+    base_query_path = os.path.dirname(query_file)
+    result = re.search(r"/mock_communities/(.*)/filter/allpath.r202.b6", query_file)
+    group_1 = result.group(1)
+    rsync_command = f"""rsync -rP {base_query_path}/allpath.r202.b6 ./mock_communities/{group_1}/filter/"""
+    print(rsync_command)
+
+
