24b1efdb49b5ea0e669d52b18428f49ca8695b00
bhillmann <hillmannben@gmail.com>
2020-11-09 16:21:26 -0600

    added the proper space for shogun_coverage.py

--- scripts/shogun_coverage.py
+++ scripts/shogun_coverage.py
@@ -13,7 +13,7 @@ b6_files = glob(f"{base_path}/mock_communities/**/filter/allpath.r202.b6", recur
 for b6_file in b6_files:
     base_path = os.path.dirname(b6_file)
 
-    command_coverage = f"""/usr/bin/time -v shogun coverage\
+    command_coverage = f"""/usr/bin/time -v shogun coverage \
 --input {b6_file} \
 --database {shogun_db} \
 --level species \
@@ -22,7 +22,7 @@ for b6_file in b6_files:
 > {base_path}/shogun.coverage.r202.log 2>&1"""
     print(command_coverage)
 
-    command_taxonomy = f"""/usr/bin/time -v shogun assign_taxonomy\
+    command_taxonomy = f"""/usr/bin/time -v shogun assign_taxonomy \
 --input {b6_file} \
 --database {shogun_db} \
 --aligner burst
@@ -32,7 +32,7 @@ for b6_file in b6_files:
 > {base_path}/shogun.taxonomy.r202.log 2>&1"""
     print(command_taxonomy)
 
-    command_capitalist = f"""/usr/bin/time -v shogun assign_taxonomy\
+    command_capitalist = f"""/usr/bin/time -v shogun assign_taxonomy \
 --input {b6_file} \
 --database {shogun_db} \
 --aligner burst
