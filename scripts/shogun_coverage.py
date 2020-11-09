f1eb576de569addfc01554e04de3ac4b393a7b24
bhillmann <hillmannben@gmail.com>
2020-11-09 16:42:33 -0600

    added the redistribute commands

--- scripts/shogun_coverage.py
+++ scripts/shogun_coverage.py
@@ -26,17 +26,31 @@ for b6_file in b6_files:
 --input {b6_file} \
 --database {shogun_db} \
 --aligner burst \
---level species \
 --no-capitalist \
 --output {base_path}/shogun.taxonomy.r202.txt \
 > {base_path}/shogun.taxonomy.r202.log 2>&1"""
     print(command_taxonomy)
 
+    command_strain_taxonomy = f"""/usr/bin/time -v shogun redistribute \
+--input {base_path}/shogun.taxonomy.r202.txt \
+--database {shogun_db} \
+--level strain \
+--output {base_path}/shogun.taxonomy.strain.r202.txt \
+> {base_path}/shogun.taxonomy.r202.strain.log 2>&1"""
+    print(command_strain_taxonomy)
+
+    command_species_taxonomy = f"""/usr/bin/time -v shogun redistribute \
+--input {base_path}/shogun.taxonomy.r202.txt \
+--database {shogun_db} \
+--level species \
+--output {base_path}/shogun.taxonomy.species.r202.txt \
+> {base_path}/shogun.taxonomy.r202.species.log 2>&1"""
+    print(command_species_taxonomy)
+
     command_capitalist = f"""/usr/bin/time -v shogun assign_taxonomy \
 --input {b6_file} \
 --database {shogun_db} \
 --aligner burst \
---level species \
 --output {base_path}/shogun.capitalist.r202.txt \
 > {base_path}/shogun.capitalist.r202.log 2>&1"""
     print(command_capitalist)
