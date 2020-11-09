f9e96e143514e060712b21b904f80373a0c33637
bhillmann <hillmannben@gmail.com>
2020-11-09 16:17:30 -0600

    added the extra coverage commands

--- scripts/shogun_coverage.py
+++ scripts/shogun_coverage.py
@@ -14,29 +14,29 @@ for b6_file in b6_files:
     base_path = os.path.dirname(b6_file)
 
     command_coverage = f"""/usr/bin/time -v shogun coverage\
-        --input {b6_file} \
-        --database {shogun_db} \
-        --level species \
-        --taxonomy_mapping \
-        -o {base_path}/coverage.r202.txt \
-        > {base_path}/shogun.coverage.r202.log 2>&1"""
+--input {b6_file} \
+--database {shogun_db} \
+--level species \
+--taxonomy_mapping \
+-o {base_path}/coverage.r202.txt \
+> {base_path}/shogun.coverage.r202.log 2>&1"""
     print(command_coverage)
 
     command_taxonomy = f"""/usr/bin/time -v shogun assign_taxonomy\
-        --input {b6_file} \
-        --database {shogun_db} \
-        --aligner burst
-        --level species \
-        --no-capitalist \
-        -o {base_path}/shogun.taxonomy.r202.txt \
-        > {base_path}/shogun.taxonomy.r202.log 2>&1"""
+--input {b6_file} \
+--database {shogun_db} \
+--aligner burst
+--level species \
+--no-capitalist \
+-o {base_path}/shogun.taxonomy.r202.txt \
+> {base_path}/shogun.taxonomy.r202.log 2>&1"""
     print(command_taxonomy)
 
     command_capitalist = f"""/usr/bin/time -v shogun assign_taxonomy\
-        --input {b6_file} \
-        --database {shogun_db} \
-        --aligner burst
-        --level species \
-        -o {base_path}/shogun.capitalist.r202.txt \
-        > {base_path}/shogun.capitalist.r202.log 2>&1"""
+--input {b6_file} \
+--database {shogun_db} \
+--aligner burst
+--level species \
+-o {base_path}/shogun.capitalist.r202.txt \
+> {base_path}/shogun.capitalist.r202.log 2>&1"""
     print(command_capitalist)
