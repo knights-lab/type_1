7f7ab754cafdf0b0ac3511815843e8d7b201ed3a
bhillmann <hillmannben@gmail.com>
2020-12-02 16:16:23 -0600

    type_1_coverage.py

--- scripts/type_1_coverage.py
+++ scripts/type_1_coverage.py
@@ -13,9 +13,9 @@ b6_files = glob(f"{base_path}/*/filter/allpath.r202.b6", recursive=True)
 for b6_file in b6_files:
     base_path = os.path.dirname(b6_file)
 
-    command_coverage = f"""/usr/bin/time -v /mnt/nvidia/pkr/code/type_1/type_1/main.py features-alignment \
+    command_coverage = f"""/usr/bin/time -v python /mnt/nvidia/pkr/code/type_1/type_1/main.py features-alignment \
 {db_features} \
 {b6_file} \
-{base_path}/coverage.t1.r202.txt \
-> {base_path}/shogun.coverage.t1.r202.log 2>&1"""
+{base_path}/t1.coverage.r202.txt \
+> {base_path}/t1.coverage.t1.r202.log 2>&1"""
     print(command_coverage)
