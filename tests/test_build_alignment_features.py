d70a3422cda77802edf1c1c152f8f06d46d435c3
bhillmann <hillmannben@gmail.com>
2020-12-03 13:56:34 -0600

    added a test for coverage of -inf (this should be impossible)

--- tests/test_build_alignment_features.py
+++ tests/test_build_alignment_features.py
@@ -10,12 +10,21 @@ from type_1.main import features_alignment
 def alignment_allpath() -> Path:
     return Path("fixtures") / Path("allpath.r202.head.b6")
 
+@pytest.fixture
+def alignment_allpath_coverage() -> Path:
+    return Path("fixtures") / Path("allpath.glitch.b6")
+
 
 @pytest.fixture()
 def database_features() -> Path:
     return Path("fixtures") / Path("db_features.csv")
 
 
-def alignment_length(alignment_allpath, database_features, tmp_path):
+def test_alignment_length(alignment_allpath, database_features, tmp_path):
     outf = tmp_path / Path("features.txt")
     features_alignment(database_features=database_features, alignment=alignment_allpath, outf=outf)
+
+
+def test_alignment_coverage(alignment_allpath_coverage, database_features, tmp_path):
+    outf = tmp_path / Path("features.txt")
+    features_alignment(database_features=database_features, alignment=alignment_allpath_coverage, outf=outf)
