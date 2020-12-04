752a41277b0ff536ba2f604afd775432f4323eb4
bhillmann <hillmannben@gmail.com>
2020-12-04 12:12:58 -0600

    fixed coverage issues with calculating fasta features

--- type_1/models.py
+++ type_1/models.py
@@ -3,7 +3,8 @@ from pydantic import BaseModel
 
 class FastaFeatures(BaseModel):
     gc_content: float
-    genome_length: int
+    total_genome_length: int
+    ungapped_genome_length: int
     num_n_groups: int
     consecutive_ns: int
     assembly_accession: str
@@ -13,8 +14,11 @@ class AlignmentFeatures(BaseModel):
     assembly_accession: str
     hits: int
     percent_coverage: float
-    expected_coverage: float
+    mean_coverage: float
+    sd_coverage: float
     percent_padded_coverage: float
+    mean_padded_coverage: float
+    expected_percent_coverage: float
     shannon_entropy: float
     percent_max_uncovered_region: float
     largest_pileup: int
